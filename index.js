162fd3c5739a978be085140200b4c6958dc9de26
Borja <borjavidesm@gmail.com>
2022-02-21 01:57:12 +0100

    Update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import config from './config.js';
 import db from './database.js';
 import City from './lib/city.js';
 import OpenWeather from './lib/providers/openweather.js';
-import day_weather from './lib/weather.js';
+import DayForecast from './lib/dayforecast.js';
 import fs from 'fs';
 import { createRequire } from 'module';
 
@@ -16,10 +16,11 @@ function getUTCEpoch(date) {
   return ~~(date.getTime() / 1000) + (date.getTimezoneOffset() * 60);
 }
 
+/*
 const now = new Date();
-const today = Object.create(day_weather);
-const tomorrow = Object.create(day_weather);
-const after_tomorrow = Object.create(day_weather);
+const today = new DayForecast();
+const tomorrow = new DayForecast();
+const after_tomorrow = new DayForecast();
 today.start_time = getUTCEpoch(now);
 today.sunrise = x.daily[0].sunrise;
 today.sunset = x.daily[0].sunset;
@@ -43,20 +44,77 @@ x.hourly.forEach(hour => {
     next = after_tomorrow;
   }
   if ((hour.dt >= current.start_time) && (hour.dt <= next.start_time)) {
-    if (hour.pop > 0) {
-      current.pop.chance = Math.max(current.pop.chance, hour.pop);
-
-      if (hour.rain["1h"].text > 0)
-        current.pop.rain_amount = Math.max(current.pop.rain_amount, hour.rain["1h"].text);
-
-      if (hour.snow["1h"].text > 0)
-        current.pop.snow_amount = Math.max(current.pop.snow_amount, hour.snow["1h"].text);
-    }
     if ((hour.dt >= current.sunrise) && (hour.dt <= current.sunset)) {
-      current.wind_speed = Math.max(current.wind_speed, hour.wind_speed);
+      current.wind.speed = Math.max(current.wind.speed, hour.wind_speed);
       current.humidity = Math.max(current.humidity, hour.humidity);
+
+      if (hour.pop > 0) {
+        current.pop.chance = Math.max(current.pop.chance, hour.pop);
+        if (!current.pop.from)
+          current.pop.from = hour.dt;
+
+        if ((hour.rain) && (hour.rain['1h'] > 0))
+          current.pop.rain_amount = Math.max(current.pop.rain_amount, hour.rain['1h']);
+
+        if ((hour.snow) && (hour.snow['1h'] > 0))
+          current.pop.snow_amount = Math.max(current.pop.snow_amount, hour.snow['1h']);
+      }
     }
   }
 });
+*/
 
-console.log(today, today.pop, tomorrow, tomorrow.pop);
+
+//console.log(today, tomorrow, '----------------');
+
+var forecast = [];
+const now = new Date();
+now.setDate(now.getDate() + 1);
+var timetom = getUTCEpoch(now);
+now.setDate(now.getDate() + 1);
+var timeaft = getUTCEpoch(now);
+
+x.daily.forEach(day => {
+  var current = new DayForecast();
+
+  current.start_time = day.dt;
+  current.sunrise = day.sunrise;
+  current.sunset = day.sunset;
+  current.temp.max_temp = day.temp.day;
+  current.temp.feels_like = day.feels_like.day;
+  current.wind.degrees = day.wind_deg;
+
+  if ((day.dt < timetom) || (day.dt < timeaft)) {
+    x.hourly
+      .filter(hour => { return ((hour.dt > current.sunrise) && (hour.dt < current.sunset)); })
+      .forEach(hour => {
+        current.wind.speed = Math.max(current.wind.speed, hour.wind_speed);
+        current.humidity = Math.max(current.humidity, hour.humidity);
+
+        if (hour.pop > 0) {
+          current.pop.chance = Math.max(current.pop.chance, hour.pop);
+          if (!current.pop.from)
+            current.pop.from = hour.dt;
+
+          if ((hour.rain) && (hour.rain['1h'] > 0))
+            current.pop.rain_amount = Math.max(current.pop.rain_amount, hour.rain['1h']);
+
+          if ((hour.snow) && (hour.snow['1h'] > 0))
+            current.pop.snow_amount = Math.max(current.pop.snow_amount, hour.snow['1h']);
+        }
+      });
+  }
+
+  else {
+    current.wind.speed = day.wind_speed;
+    current.humidity = day.humidity;
+    if (day.pop > 0) {
+      current.pop.chance = day.pop;
+      if (day.rain) current.pop.rain_amount = day.rain;
+      if (day.snow) current.pop.snow_amount = day.snow;
+    }
+  }
+  forecast.push(current);
+});
+
+console.log(forecast);
