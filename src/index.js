8ea8af7f6bab42bd08baa594f76cc08f7558402f
Borja <borjavidesm@gmail.com>
2022-06-23 03:01:26 +0200

    Adding package-lock.json to the commits

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app.js';
-
 import settings from './settings.js';
 
 const container = document.getElementById('root');
